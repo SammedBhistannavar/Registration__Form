0f36629aeb9f1793a47ac07580d7f11c9d14c4d6
sammed <sammedsb71@gmail.com>
2024-02-14 13:40:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const dotenv  = require("dotenv");
+
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+
+const username = process.env.USER_NAME;
+const userpassword = process.env.USER_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${userpassword}@cluster0.qlzyu9t.mongodb.net/registrationformDB`,{
+    useNewUrlParser :true,
+    useUnifiedTopology :true 
+});
+module.exports = mongoose.connection;
+
+//Registration Schema
+const registrationschema = new mongoose.Schema({
+    name :String,
+    email :  String,
+    password : String 
+});
+
+//module of Schema
+const Registration = mongoose.model("Registration",registrationschema);
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+app.get("/", (req,res) =>{
+    res.sendFile(__dirname+'/pages/index.html');
+
+});
+app.post("/register",async(req,res)=>{
+       
+   try {
+    const {name,email,password} = req.body;
+        const registrationdata = new Registration({
+            name,
+            email,
+            password,
+        });
+       await registrationdata.save();
+       res.redirect("/sucess");
+
+}
+    catch (error) {
+        console.log(error);
+       }});
+
+app.get("/sucess",(req,res) =>{
+    res.sendFile(__dirname + "/pages/sucessfullpage.html");
+})
+
+app.listen(port,function(){
+    console.log(`Server is Runing on ${port}`);
+    });
